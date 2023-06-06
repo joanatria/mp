3c42bfbab03de81b4fd3bba4d780d565ec334b58
jstria1 <jstria1@up.edu.ph>
2023-06-06 10:47:56 +0800

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://o.remove.bg/downloads/d488b170-fe14-4c8c-9136-547c42814cfc/photo-1589465885857-44edb59bbff2-removebg-preview.png",
+        title: "SUMMER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcfdff",
+    },
+    {
+        id: 2,
+        img: "https://o.remove.bg/downloads/d488b170-fe14-4c8c-9136-547c42814cfc/photo-1589465885857-44edb59bbff2-removebg-preview.png",
+        title: "WINTER SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f2ebdd",
+    },
+    {
+        id: 3,
+        img: "https://o.remove.bg/downloads/d488b170-fe14-4c8c-9136-547c42814cfc/photo-1589465885857-44edb59bbff2-removebg-preview.png",
+        title: "POPULAR SALE",
+        description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f1e9d2",
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const App = lazy(() => import('./App'));
+
+// This is the root component
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    {/* The Provider component accepts a value prop to be passed to consuming components that are descendants of this Provider. */}
+      {/* Routers is used to separate diffrent screen using thier url */}
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<App />}>
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
+  </div>
+  
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
